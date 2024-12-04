b91a2abd3403595f76c45c37426bb2df020e550f
Noxius18 <akagiadam@gmail.com>
2024-12-04 07:43:54 +0700

    Refactoring Seeder + seeder untuk kategori

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,22 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::create([
-            'nama' => 'Administrator',
-            'email' => 'admin@gmail.com',
-            'role' => '1',
-            'status' => 1,
-            'hp' => '0812345678901',
-            'password' => bcrypt('P@55word')
-        ]);
-
-        User::create([
-            'nama' => 'Sopian Aji',
-            'email' => 'sopian4ji@gmail.com',
-            'role' => '0',
-            'status' => 1,
-            'hp' => '081234567892',
-            'password' => bcrypt('P@55word')
-        ]);
+        $this->call(UserSeeder::class);
+        $this->call(KategoriSeeder::class);
     }
 }
